cd7287e655069e61ab56d4b74f4e431e95804e2f
Eva Lateltin <evalateltin@gmail.com>
2023-06-02 22:15:57 +0200

    Feat(search) add search function on submit

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,25 +1,53 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 import attractions from "../../assets/attractions.svg";
 import InputField from "../../elements/InputField";
 import SearchButton from "../../elements/SearchButton";
+import { getCities } from "../../services/cities";
+import { MessageContext } from "../../contexts/message";
+import { TokenContext } from "../../contexts/token";
+import { CitiesContext } from "../../contexts/cities";
 import {
   buttonContainer,
   container,
   inputContainer,
+  disableSelect,
 } from "./style.module.scss";
 
 export default function SearchBar() {
   const [input, setInput] = useState("");
+  const { token } = useContext(TokenContext);
+  const { setMessage } = useContext(MessageContext);
+  const { setCities } = useContext(CitiesContext);
+
+  function handleSearch() {
+    if (input.length >= 3) {
+      getCities(token, input).then((data) => {
+        if (data) {
+          setCities(data);
+        } else {
+          setCities([]);
+          setMessage("City not found");
+        }
+      });
+    } else {
+      setCities([]);
+      setMessage("Search for a city");
+    }
+  }
 
   return (
     <div className={container}>
       <div className={inputContainer}>
-        <img src={attractions} />
-        <InputField input={input} setInput={setInput} />
+        <img src={attractions} className={disableSelect} />
+        <InputField
+          input={input}
+          setInput={setInput}
+          handleSearch={handleSearch}
+        />
       </div>
       <div className={buttonContainer}>
-        <SearchButton input={input} />
+        <SearchButton input={input} handleSearch={handleSearch} />
       </div>
     </div>
   );
