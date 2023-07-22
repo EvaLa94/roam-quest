5ee5ee90fbc5fb75468071896d02e238de3c8fb6
Eva Lateltin <evalateltin@gmail.com>
2023-07-22 17:06:20 +0200

    Refactor: move router in separated file

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
 import TokenProvider from "@/contexts/token";
-import Attractions from "@/pages/Attractions";
-import Search from "@/pages/Search";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-
-import RootLayout from "./layout/RootLayout";
+import RootRouter from "@/router/RootRouter";
 
 import "./App.scss";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<Search />} />
-      <Route path="attractions" element={<Attractions />} />
-    </Route>
-  )
-);
 
 export default function App() {
   return (
     <>
       <TokenProvider>
-        <RouterProvider router={router} />
+        <RootRouter />
       </TokenProvider>
     </>
   );
